85b2c623b84f47edcfa55e5b231cfececf6db0b9
Farhan124xx <91053797+Farhan124xx@users.noreply.github.com>
2021-12-13 07:06:44 +0800

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -905,7 +905,11 @@ class PlayState extends MusicBeatState
 		doof.cameras = [camHUD];
 		doof.scrollFactor.set();
 		thecutscene(doof);
-		
+		creditTxt = new FlxText(876, 648, 348);
+                creditTxt.text = 'PORTED BY\nFarhan124xx';
+                                creditTxt.setFormat(Paths.font("vcr"), 30, FlxColor.WHITE, RIGHT);
+                                                creditTxt.setBorderStyle(OUTLINE, FlxColor.BLACK, 3, 1);       
+                                                                creditTxt.scrollFactor.set();
 	}
 	
 	function churchIntro2()
